2496b50b6006af02e44360d2ca796ba336cee5db
Bluedin <pacome.chulejeune@viacesi.fr>
2017-10-26 15:40:25 +0200

    Reconfiguration of simulation speed

--- src/exia/fr/Simulation.java
+++ src/exia/fr/Simulation.java
@@ -79,7 +79,7 @@ public class Simulation implements Runnable{
 	public void run() {
 		while(true) {
 			try {
-				Thread.sleep(100);
+				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
